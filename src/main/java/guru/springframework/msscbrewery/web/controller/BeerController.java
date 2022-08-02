35cf8208d89a55d349b05494d378a54762a530f9
master <powerstate@outlook,de>
2022-08-02 11:23:53 +0200

    add requestBody and tests

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -28,7 +28,7 @@ public class BeerController {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
     @PostMapping // POST - create new beer
-    public ResponseEntity handlePost(BeerDto beerDto){
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -37,6 +37,14 @@ public class BeerController {
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
 }
